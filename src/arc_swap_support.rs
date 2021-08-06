6bc407fba447797c6bc16b5973a09b25d54f994d
Jiahao XU <Jiahao_XU@outlook.com>
2021-08-06 20:43:55 +1000

    Impl `RefCnt` for `Arc<T>`
    Signed-off-by: Jiahao XU <Jiahao_XU@outlook.com>

--- src/arc_swap_support.rs
+++ src/arc_swap_support.rs
@@ -2,7 +2,7 @@ use arc_swap::RefCnt;
 
 use core::ffi::c_void;
 use core::ptr::NonNull;
-use crate::ThinArc;
+use crate::{Arc, ThinArc};
 
 unsafe impl<H, T> RefCnt for ThinArc<H, T> {
     type Base = c_void;
@@ -20,5 +20,24 @@ unsafe impl<H, T> RefCnt for ThinArc<H, T> {
     #[inline]
     unsafe fn from_ptr(ptr: *const Self::Base) -> ThinArc<H, T> {
         ThinArc::from_raw(NonNull::new_unchecked(ptr as *mut _))
+    }
+}
+
+unsafe impl<T> RefCnt for Arc<T> {
+    type Base = T;
+
+    #[inline]
+    fn into_ptr(me: Self) -> *mut Self::Base {
+        Arc::into_raw(me) as *mut _
+    }
+
+    #[inline]
+    fn as_ptr(me: &Self) -> *mut Self::Base {
+        Arc::as_ptr(me) as *mut _
+    }
+
+    #[inline]
+    unsafe fn from_ptr(ptr: *const Self::Base) -> Self {
+        Arc::from_raw(ptr)
     }
 }
