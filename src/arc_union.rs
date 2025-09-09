5ae7e641e20d6153ceefd0ff7f9a6b7dc159861e
Ifeanyi Orizu <iorizu1811@gmail.com>
2025-09-09 11:03:51 -0500

    Add elided lifetimes to return types

--- src/arc_union.rs
+++ src/arc_union.rs
@@ -62,7 +62,7 @@ impl<A, B> ArcUnion<A, B> {
     }
 
     /// Returns an enum representing a borrow of either A or B.
-    pub fn borrow(&self) -> ArcUnionBorrow<A, B> {
+    pub fn borrow(&self) -> ArcUnionBorrow<'_, A, B> {
         if self.is_first() {
             let ptr = self.p.as_ptr() as *const A;
             let borrow = unsafe { ArcBorrow::from_ptr(ptr) };
@@ -99,7 +99,7 @@ impl<A, B> ArcUnion<A, B> {
     }
 
     /// Returns a borrow of the first type if applicable, otherwise `None`.
-    pub fn as_first(&self) -> Option<ArcBorrow<A>> {
+    pub fn as_first(&self) -> Option<ArcBorrow<'_, A>> {
         match self.borrow() {
             ArcUnionBorrow::First(x) => Some(x),
             ArcUnionBorrow::Second(_) => None,
@@ -107,7 +107,7 @@ impl<A, B> ArcUnion<A, B> {
     }
 
     /// Returns a borrow of the second type if applicable, otherwise None.
-    pub fn as_second(&self) -> Option<ArcBorrow<B>> {
+    pub fn as_second(&self) -> Option<ArcBorrow<'_, B>> {
         match self.borrow() {
             ArcUnionBorrow::First(_) => None,
             ArcUnionBorrow::Second(x) => Some(x),
